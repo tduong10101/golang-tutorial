46f304ff179e114a89a9faea578f3812abcec308
terry duong <tduong10101@gmail.com>
2024-03-03 00:45:14 +1100

    add lesson-2

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	// strings
+	var nameOne string = "mario"
+	nameTwo := "luigi"
+
+	fmt.Println(nameOne, nameTwo)
+
+  //ints
+  var ageOne int = 20
+  var ageTwo = 30
+  ageThree := 40
+
+  fmt.Println(ageOne, ageTwo, ageThree)
 }

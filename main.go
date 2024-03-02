dce290ce8d30966307d29f9a649cc3fbc6f57af6
terry duong <tduong10101@gmail.com>
2024-03-03 01:08:47 +1100

    update lesson-2

--- main.go
+++ main.go
@@ -15,4 +15,13 @@ func main() {
   ageThree := 40
 
   fmt.Println(ageOne, ageTwo, ageThree)
+
+  // bits & memory
+  // var numbOne int8 = 25
+  // var numbTwo int8 = -128
+  // var numbThree uint16 = 256
+  
+  var scoreOne float32 = 25.98
+  var scoreTwo float62 = 87581945791083475901875.7
+  scoreThree := 25.6
 }

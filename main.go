fa2bfc04bf1685536d98a4722f61a3f3d4b868f0
terry duong <tduong10101@gmail.com>
2024-03-03 01:10:28 +1100

    reformat

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ func main() {
 
 	fmt.Println(nameOne, nameTwo)
 
-  //ints
-  var ageOne int = 20
-  var ageTwo = 30
-  ageThree := 40
+	// ints
+	var ageOne int = 20
+	ageTwo := 30
+	ageThree := 40
 
-  fmt.Println(ageOne, ageTwo, ageThree)
+	fmt.Println(ageOne, ageTwo, ageThree)
 
-  // bits & memory
-  // var numbOne int8 = 25
-  // var numbTwo int8 = -128
-  // var numbThree uint16 = 256
-  
-  var scoreOne float32 = 25.98
-  var scoreTwo float62 = 87581945791083475901875.7
-  scoreThree := 25.6
+	// bits & memory
+	// var numbOne int8 = 25
+	// var numbTwo int8 = -128
+	// var numbThree uint16 = 256
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float62 = 87581945791083475901875.7
+	scoreThree := 25.6
 }

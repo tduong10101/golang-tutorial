0f762bb79383e9e625ea5a1f2dd9a93f364cafd7
terry duong <tduong10101@gmail.com>
2024-03-04 21:17:08 +1100

    lesson-7

--- main.go
+++ main.go
@@ -3,27 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	//   fmt.Println("value of x is", x)
-	//   x++
-	// }
+	age := 25
 
-  names := []string{"mario", "luigi", "yoshi", "peach"}
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
 
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
 	}
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at ps", index)
+			break
+		}
 
-  for index, value := range names {
-    fmt.Printf("the value at index %v is %v\n", index, value)
-  }
-  for _, value := range names {
-    fmt.Printf("the value is %v\n", value)
-  }
-  for index, value := range names {
-    fmt.Printf("the value is %v\n", value)
-    names[index] = "delete_name_" + value 
-  }
-  fmt.Println(names)
+		fmt.Printf("the value at pos %v is %v\n", index, value)
+	}
 }

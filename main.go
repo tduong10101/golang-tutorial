f4727c20b0fc3f2d6c1da6c67c18bb0e58cb81ec
terry duong <tduong10101@gmail.com>
2024-03-05 21:12:37 +1100

    lesson-12

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import "fmt"
 
-var score = 90.5
-
 func main() {
-	sayHello("mario")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":          4.99,
+		"pie":           7.99,
+		"salad":         6.99,
+		"coffe pudding": 3.55,
 	}
-  showScore()
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		2657397495: "mario",
+		3758019485: "luigi",
+		2765978590: "peach",
+	}
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[2765978590])
+
+	phonebook[2765978375] = "bowser"
+
+	phonebook[3758019485] = "yoshi"
+
+  fmt.Println(phonebook)
 }

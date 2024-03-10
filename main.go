9d541fc802237008e4d252e7c92e9a892261127d
terry duong <tduong10101@gmail.com>
2024-03-10 12:09:30 +1100

    lesson-15

--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import "fmt"
 
-func updateName(x *string) {
-	*x = "wedge"
-}
-
 func main() {
-	name := "tifa"
-	fmt.Println(name)
+  mybill := newBill("mario's Bill")
 
-	// fmt.Println("memory address of name is:", &name)
-
-	m := &name
-	// fmt.Println("memory address:", m)
-	// fmt.Println("value at memory address:", *m)
-
-	updateName(m)
-	fmt.Println(name)
+  fmt.Println(mybill)
 }

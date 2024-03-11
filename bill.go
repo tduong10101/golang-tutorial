b2d7181c235d2bcc81d625745a43c3c1adbaf118
terry duong <tduong10101@gmail.com>
2024-03-11 19:41:09 +1100

    lesson-22

--- bill.go
+++ bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -21,30 +24,41 @@ func newBill(name string) bill {
 
 // format the bill
 func (b *bill) format() string {
-  fs := "Bill breakdown: \n"
-  var total float64 = 0
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
 
-  // list items
-  for k,v := range b.items {
-    fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
-    total += v
-  }
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
+		total += v
+	}
 
-  // add tip
-  fs += fmt.Sprintf("%-25v ...$%0.2f\n", "tip:", b.tip)
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%0.2f\n", "tip:", b.tip)
 
-  // total
-  fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
+	// total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
-  return fs
+	return fs
 }
 
 // update tip
-func (b *bill) updateTip(tip float64)  {
-  b.tip = tip
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
 }
 
 // add an item to the bill
-func (b *bill) addItem(name string, price float64)  {
-  b.items[name] = price
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+// save bill
+func (b *bill) save() {
+	data := []byte(b.format())
+  os.Mkdir("bills/", os.ModePerm)
+  err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("bill was saved to file")
 }
